528c05f10f0509713a6b0081e837cfc76eb6a31d
Chyld Medford <chyld.medford@gmail.com>
2014-09-17 15:58:36 -0500

    done

---
+++ client/components/directives/cm-movie/cm-movie.js
@@ -0,0 +1,33 @@
+/* jshint camelcase:false */
+
+(function(){
+  'use strict';
+
+  angular.module('cmMovieModule', [])
+  .factory('MovieApi', ['$http', function($http){
+    function info(title){
+      return $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=' + title + '&page_limit=1&page=1&apikey=3mhvmefydmkre6dx2c959h6q&callback=JSON_CALLBACK');
+    }
+
+    return {info:info};
+  }])
+  .directive('cmMovie', [function(){
+    var o = {};
+
+    o.restrict    = 'A';
+    o.templateUrl = '/components/directives/cm-movie/cm-movie.html';
+    o.scope       = {title:'@'};
+    o.link        = function(scope, element, attrs){
+                    };
+
+    o.controller  = ['$scope', 'MovieApi', function($scope, MovieApi){
+                      MovieApi.info($scope.title).then(function(response){
+                        $scope.movie = response.data.movies[0];
+                        $scope.poster = $scope.movie.posters.thumbnail.replace(/_tmb/, '_pos');
+                      });
+                    }];
+
+    return o;
+  }]);
+})();
+
