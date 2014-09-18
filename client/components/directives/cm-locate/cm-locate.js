82b5f6530ef8a869811e424eb95adc2cda951358
Chyld Medford <chyld.medford@gmail.com>
2014-09-18 12:14:22 -0500

    locate

---
+++ client/components/directives/cm-locate/cm-locate.js
@@ -0,0 +1,45 @@
+(function(){
+  'use strict';
+
+  angular.module('cmLocateModule', [])
+  .factory('LocationService', ['$q', function($q){
+
+    function locate(){
+      var deferred = $q.defer(),
+          options  = {enableHighAccuracy: true, timeout: 10000, maximumAge: 0};
+
+      navigator.geolocation.getCurrentPosition(deferred.resolve, deferred.reject, options);
+
+      return deferred.promise;
+    }
+
+    return {locate:locate};
+
+  }])
+  .directive('cmLocate', [function(){
+    var o = {};
+
+    o.restrict    = 'A';
+    o.templateUrl = '/components/directives/cm-locate/cm-locate.html';
+    o.scope       = {};
+    o.link        = function(scope, element, attrs){
+                    };
+
+    o.controller  = ['$scope', 'LocationService', function($scope, LocationService){
+                      $scope.findMe = function(){
+                        LocationService.locate().then(success, error);
+                      };
+
+                      function success(pos){
+                        console.log(pos);
+                      }
+
+                      function error(err){
+                        console.log(err);
+                      }
+                    }];
+
+    return o;
+  }]);
+})();
+
