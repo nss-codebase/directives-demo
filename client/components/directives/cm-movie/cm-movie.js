ba291056aa6fb4ef8b76525fcdf9f2ab1c83dd40
Chyld Medford <chyld.medford@gmail.com>
2014-09-18 10:11:57 -0500

    add and del movie

--- client/components/directives/cm-movie/cm-movie.js
+++ client/components/directives/cm-movie/cm-movie.js
@@ -16,7 +16,7 @@
 
     o.restrict    = 'A';
     o.templateUrl = '/components/directives/cm-movie/cm-movie.html';
-    o.scope       = {title:'@'};
+    o.scope       = {title:'@', remove:'&'};
     o.link        = function(scope, element, attrs){
                     };
 
--- client/views/home/home.js
+++ client/views/home/home.js
@@ -5,6 +5,16 @@
   .controller('HomeCtrl', ['$scope', '$interval', 'Home', function($scope, $interval, Home){
     $scope.people = [{name:'Bob', age:25}, {name:'Sally', age:22}];
     $scope.symbols = ['AAPL', 'GOOG', 'MSFT', 'AMZN'];
+    $scope.titles = ['The Hobbit'];
+
+    $scope.delMovie = function(index){
+      $scope.titles.splice(index, 1);
+    };
+
+    $scope.addMovie = function(){
+      $scope.titles.push($scope.title);
+      $scope.title = null;
+    };
   }]);
 })();
 
